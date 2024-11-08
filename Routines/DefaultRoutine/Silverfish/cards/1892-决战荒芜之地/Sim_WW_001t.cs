cc6f9f563b7d63de9b321cfcf8270d5bf4781e55
Jagger <>
2024-11-09 00:06:31 +0800

    Add:实现Sim_DEEP_999t1和Sim_WW_001t战吼效果

--- Routines/DefaultRoutine/Silverfish/cards/1892-决战荒芜之地/Sim_WW_001t.cs
+++ Routines/DefaultRoutine/Silverfish/cards/1892-决战荒芜之地/Sim_WW_001t.cs
@@ -11,7 +11,17 @@ namespace HREngine.Bots
 	//造成$3点伤害。
 	class Sim_WW_001t : SimTemplate
 	{
-		
-		
+		public override void onCardPlay(Playfield p, bool ownplay, Minion target, int choice)
+		{
+			// 对目标造成3点伤害
+			p.minionGetDamageOrHeal(target, 3);
+		}
+
+		public override PlayReq[] GetPlayReqs()
+		{
+			return new PlayReq[] {
+				new PlayReq(CardDB.ErrorType2. REQ_TARGET_TO_PLAY),
+			};
+		}
 	}
 }
