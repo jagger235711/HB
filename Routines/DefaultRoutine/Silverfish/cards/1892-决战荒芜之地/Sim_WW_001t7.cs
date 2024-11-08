45aed4dae1c08f5b6c6c3a70e5be983bb33c3479
Jagger <>
2024-11-08 19:32:38 +0800

    Add:实现Sim_WW_001t7战吼效果，修复敌方随从返回手牌逻辑

--- Routines/DefaultRoutine/Silverfish/cards/1892-决战荒芜之地/Sim_WW_001t7.cs
+++ Routines/DefaultRoutine/Silverfish/cards/1892-决战荒芜之地/Sim_WW_001t7.cs
@@ -11,7 +11,23 @@ namespace HREngine.Bots
 	//<b>战吼：</b>将一个敌方随从移回其拥有者的手牌。
 	class Sim_WW_001t7 : SimTemplate
 	{
-		
-		
+		public override void getBattlecryEffect(Playfield p, Minion own, Minion target, int choice)
+		{
+			// 检查是否有敌方随从可以作为目标
+			if (target != null && !target.own)
+			{
+				// 将敌方随从移回其拥有者的手牌
+				p.minionReturnToDeck(target, target.own);
+			}
+		}
+		public override PlayReq[] GetPlayReqs()
+		{
+			// 扰魔是不吃法术和英雄技能的指定 但是随从技能没办法 参考萨隆偷带扰魔的奇利亚斯
+			return new PlayReq[] {
+				new PlayReq(CardDB.ErrorType2.REQ_MINIMUM_ENEMY_MINIONS, 1),
+				new PlayReq(CardDB.ErrorType2.REQ_MINION_TARGET),
+				new PlayReq(CardDB.ErrorType2.REQ_ENEMY_TARGET),
+			};
+		}
 	}
 }
