0e1872146ce845b4d77afd84f5b6ab6e0128dd89
Yuanzhilx <884121435@qq.com>
2024-11-08 15:51:49 +0800

    【轻量级 PR】：update Routines/DefaultRoutine/Silverfish/cards/1935-深暗领域/Sim_GDB_464

--- Routines/DefaultRoutine/Silverfish/cards/1935-深暗领域/Sim_GDB_464.cs
+++ Routines/DefaultRoutine/Silverfish/cards/1935-深暗领域/Sim_GDB_464.cs
@@ -11,7 +11,24 @@ namespace HREngine.Bots
 	//将每个随从的攻击力和生命值变为两者中的低值。
 	class Sim_GDB_464 : SimTemplate
 	{
-		
-		
+		public override void onCardPlay(Playfield p, bool ownplay, Minion target, int choice)
+        {
+            foreach (Minion m in p.ownMinions)//判断我方场面
+            {
+                if (m.Angr > m.Hp){
+                    p.minionSetAngrToX(m, m.Hp);//如果随从攻击力＞生命值，将攻击力置为生命值的数值
+                }else{
+                    p.minionSetLifetoX(m, m.Angr);//如果随从攻击力<=生命值，将置生命值为攻击力的数值
+                }
+            }
+            foreach (Minion m in p.enemyMinions)//判断敌方场面
+            {
+                if (m.Angr > m.Hp){
+                    p.minionSetAngrToX(m, m.Hp);//如果随从攻击力＞生命值，将攻击力置为生命值的数值
+                }else{
+                    p.minionSetLifetoX(m, m.Angr);//如果随从攻击力<=生命值，将置生命值为攻击力的数值
+                }
+            }
+        }
 	}
 }
