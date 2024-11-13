45940b0063d9714a36df22cc56dc987016468d93
Jagger <>
2024-11-13 12:58:46 +0800

    Refactor: 统一横扫卡牌类继承及代码格式
    (cherry picked from commit e1e765b81491e50f81e472b90edc0e2a0ead740e)

--- Routines/DefaultRoutine/Silverfish/cards/1635-怀旧/Sim_CS2_012.cs
+++ Routines/DefaultRoutine/Silverfish/cards/1635-怀旧/Sim_CS2_012.cs
@@ -4,15 +4,15 @@ using System.Text;
 
 namespace HREngine.Bots
 {
-	class Sim_CS2_012 : SimTemplate //* 横扫 Swipe
-	{
-		//Deal $4 damage to an enemy and $1 damage to all other enemies.
-		//对一个敌人造成$4点伤害，并对所有其他敌人造成$1点伤害。
+    class Sim_CS2_012 : SimTemplate //* 横扫 Swipe
+    {
+        //Deal $4 damage to an enemy and $1 damage to all other enemies.
+        //对一个敌人造成$4点伤害，并对所有其他敌人造成$1点伤害。
 
-		public override void onCardPlay(Playfield p, bool ownplay, Minion target, int choice)
-		{
+        public override void onCardPlay(Playfield p, bool ownplay, Minion target, int choice)
+        {
             int dmg = (ownplay) ? p.getSpellDamageDamage(4) : p.getEnemySpellDamageDamage(4);
-            int dmg1 = (ownplay)? p.getSpellDamageDamage(1) : p.getEnemySpellDamageDamage(1);
+            int dmg1 = (ownplay) ? p.getSpellDamageDamage(1) : p.getEnemySpellDamageDamage(1);
 
             List<Minion> temp = (ownplay) ? p.enemyMinions : p.ownMinions;
             p.minionGetDamageOrHeal(target, dmg);
@@ -39,7 +39,7 @@ namespace HREngine.Bots
                 }
                 if (Hrtprozis.Instance.ownMinions.Count > p.ownMinions.Count) p.lostDamage += dmg1;
             }
-		}
+        }
 
         public override PlayReq[] GetPlayReqs()
         {
@@ -48,5 +48,5 @@ namespace HREngine.Bots
                 new PlayReq(CardDB.ErrorType2.REQ_ENEMY_TARGET),
             };
         }
-	}
+    }
 }
