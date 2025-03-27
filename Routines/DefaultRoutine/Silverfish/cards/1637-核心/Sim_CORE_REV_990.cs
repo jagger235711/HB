48b17b942f74f9b512cea4301e6c5ff33ee8dc60
琴弦上的宇宙 <913605201@qq.com>
2025-03-27 23:55:39 +0800

    V1.0.8

--- Routines/DefaultRoutine/Silverfish/cards/1637-核心/Sim_CORE_REV_990.cs
+++ Routines/DefaultRoutine/Silverfish/cards/1637-核心/Sim_CORE_REV_990.cs
@@ -9,7 +9,7 @@ namespace HREngine.Bots
 	//赤红深渊
 	//[x]Deal 1 damage to a minion and give it +2 Attack.
 	//对一个随从造成1点伤害，并使其获得+2攻击力。
-	class Sim_REV_990 : SimTemplate
+	class Sim_CORE_REV_990 : SimTemplate
 	{
         public override void useLocation(Playfield p, Minion triggerMinion, Minion target)
         {
